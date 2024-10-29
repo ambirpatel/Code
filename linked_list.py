6e84e46e112ad63c26f233fb587ca6b9e0a55b0d
Ambir <47106756+ambirpatel@users.noreply.github.com>
2024-10-29 23:34:30 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -74,17 +74,16 @@ class LinkedList:
 	def append(self, value):
 
 		new_node = Node(value)
+		# empty list check
 		if self.head == None:
 			self.head = new_node
-			self.n = self.n + 1
-			return
+		else:
+			curr = self.head()
+			while curr.next != None:
+				curr = curr.next
 
-		curr = self.head()
-		while curr.next != None:
-			curr = curr.next
-
-		# You are at last node
-		curr.next = new_node
+			# You are at last node
+			curr.next = new_node
 		self.n = self.n + 1
 
 	def insert_after(self, after, value):
